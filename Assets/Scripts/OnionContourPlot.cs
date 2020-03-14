a03d14bf087ddf339c4078cf82d0ee639314f296
mryamz <mryamz@users.noreply.github.com>
2020-03-14 11:00:07 -0700

    added negative contour plot of onion

--- Assets/Scripts/OnionContourPlot.cs
+++ Assets/Scripts/OnionContourPlot.cs
@@ -10,6 +10,8 @@ public class OnionContourPlot
     private float radius;
     private float step;
 
+    private Vector3 position;
+
     /**
      * @params
      * 
@@ -18,10 +20,11 @@ public class OnionContourPlot
      * step: is used to change resolution of plot
      * 
      */
-    public OnionContourPlot(float radius, float step)
+    public OnionContourPlot(Vector3 position, float radius, float step)
     {
         this.radius = radius;
         this.step = step;
+        this.position = position;
     }
 
     /**
@@ -34,18 +37,26 @@ public class OnionContourPlot
      * 
      * radial_scaler: changes the plane-size at each layer
      * 
+     * step: overrides step member for layer specific calls
+     * 
      * 
      */
     public void addContour(float c, float vertical_offset, float radial_scaler)
+    {
+
+        addContour(c, vertical_offset, radial_scaler, step);
+    }
+
+    public void addContour(float c, float vertical_offset, float radial_scaler, float step)
     {
 
         List<Vector3> layer = new List<Vector3>();
 
         float radius = this.radius * radial_scaler;
 
-        for(float x = -radius; x < radius; x += step)
+        for (float x = -radius; x < radius; x += step)
         {
-            for(float y = -radius; y < radius; y += step)
+            for (float y = -radius; y < radius; y += step)
             {
                 if (Mathf.Sqrt((x * x) + (y * y)) < radius)
                 {
@@ -54,15 +65,15 @@ public class OnionContourPlot
                     float z = c + (x * x) + (y * y);
 
                     // ensure we don't create an imaginary number
-                    if(z > 0)
+                    if (z > 0)
                     {
                         float answer = Mathf.Sqrt(z);
 
                         answer += vertical_offset;
 
                         // answer is plus or minus sqrt of z
-                        layer.Add(new Vector3(x, answer, y));
-                        layer.Add(new Vector3(x, -answer, y));
+                        layer.Add(new Vector3(x, answer, y) + position);
+                        layer.Add(new Vector3(x, -answer, y) + position);
 
                         // switch z and y axis for unity.
                         // in math z is up, in unity y is up
@@ -76,4 +87,5 @@ public class OnionContourPlot
 
         contours.Add(layer);
     }
+
 }
--- Assets/Scripts/PlotTestDriver.cs
+++ Assets/Scripts/PlotTestDriver.cs
@@ -9,25 +9,36 @@ public class PlotTestDriver : MonoBehaviour
 
     void Start()
     {
-
         float step_size = 0.1f;
-        OnionContourPlot plot = new OnionContourPlot(1, step_size);
         point_data.transform.localScale = new Vector3(step_size, step_size, step_size);
 
-        plot.addContour(0, 0, 1);
-        plot.addContour(1, .5f, 1.2f);
-        plot.addContour(2, 1, 1.4f);
+
+        OnionContourPlot plot1 = new OnionContourPlot(new Vector3(0, 0, 0), 1, step_size);
+        plot1.addContour(0, 0, 1);
+        plot1.addContour(1, .5f, 1.2f);
+        plot1.addContour(2, 1, 1.4f);
+        plot(plot1, point_data);
+
+        step_size = 0.03f;
+        OnionContourPlot plot2 = new OnionContourPlot(new Vector3(0, 0, 0), 1, step_size);
+        plot2.addContour(-1, 2, 1.4f);
+        plot2.addContour(-2, 1, 1.8f);
+        plot2.addContour(-3, 0, 2.2f);
+
+        plot(plot2, point_data);
 
 
 
+    }
+
+    public static void plot(OnionContourPlot plot, GameObject point_data)
+    {
         for (int i = 0; i < plot.contours.Count; i++)
         {
-            for(int j = 0; j < plot.contours[i].Count; j++)
+            for (int j = 0; j < plot.contours[i].Count; j++)
             {
                 Instantiate(point_data, plot.contours[i][j], Quaternion.identity);
             }
         }
-
-
     }
 }
