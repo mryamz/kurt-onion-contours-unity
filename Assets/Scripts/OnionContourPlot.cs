b3aec9c1209b5e46d920f1b3873f6d97666226d1
mryamz <mryamz@users.noreply.github.com>
2020-03-13 22:36:46 -0700

    init

---
+++ Assets/Scripts/OnionContourPlot.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnionContourPlot
+{
+
+    public List<List<Vector3>> contours = new List<List<Vector3>>();
+
+    private float radius;
+    private float step;
+    private float layer_padding;
+
+    public OnionContourPlot(float radius, float step, float layer_padding)
+    {
+        this.radius = radius;
+        this.step = step;
+        this.layer_padding = layer_padding;
+    }
+
+    /*
+     * 
+     * c represents the contour value
+     * 
+     * vertical_offset represents layer spacing
+     * 
+     * radial_scaler changes the plane-size at each layer
+     * 
+     * 
+     */
+    public void addContour(float c, float vertical_offset, float radial_scaler)
+    {
+
+        List<Vector3> layer = new List<Vector3>();
+
+        float radius = this.radius * radial_scaler;
+
+        for(float x = -radius; x < radius; x += step)
+        {
+            for(float y = -radius; y < radius; y += step)
+            {
+                if (Mathf.Sqrt((x * x) + (y * y)) < radius)
+                {
+
+                    // solve for z in z^2 = contour + x^2 + y^2
+                    float z = c + (x * x) + (y * y);
+
+                    // ensure we don't create an imaginary number
+                    if(z > 0)
+                    {
+                        float answer = Mathf.Sqrt(z);
+
+                        answer += vertical_offset;
+
+                        // answer is plus or minus sqrt of z
+                        layer.Add(new Vector3(x, answer, y));
+                        layer.Add(new Vector3(x, -answer, y));
+
+                        // switch z and y axis for unity.
+                        // in math z is up, in unity y is up
+                    }
+
+                }
+
+
+            }
+        }
+
+        contours.Add(layer);
+    }
+}
---
+++ Assets/Scripts/PlotTestDriver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotTestDriver : MonoBehaviour
+{
+
+    public GameObject point_data;
+
+    void Start()
+    {
+
+        OnionContourPlot plot = new OnionContourPlot(10, 1, 5);
+
+        plot.addContour(0, 0, 1);
+        plot.addContour(1, 3, 1.2f);
+        plot.addContour(2, 6, 1.4f);
+
+
+
+        for (int i = 0; i < plot.contours.Count; i++)
+        {
+            for(int j = 0; j < plot.contours[i].Count; j++)
+            {
+                Instantiate(point_data, plot.contours[i][j], Quaternion.identity);
+            }
+        }
+
+
+    }
+}
